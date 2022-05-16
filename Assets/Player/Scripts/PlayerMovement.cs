401bbfb5de7c3480f3d886d39682b70952ecb181
angel_03 <angel20dsouza00@gmail.com>
2022-05-16 23:59:21 +0530

    player Sprites

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float direction;
+    public GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveRight()
+    {
+       
+    }
+    public void MoveLeft()
+    {
+         gameObject.transform.Translate(new Vector3(-1,0,0) * speed * Time.deltaTime);
+    }
+
+    Void FixedUpdate()
+    {
+        
+    }
+}
