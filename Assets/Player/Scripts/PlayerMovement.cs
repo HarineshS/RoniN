b1bc5ea6840cd603f66ee4d97a60c44a7615c3ea
angel_03 <angel20dsouza00@gmail.com>
2022-05-18 23:10:58 +0530

    player Movement and jump

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -4,33 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public float direction;
-    public GameObject player;
+    private Rigidbody2D rb;
+    //private Animator animator;
+    private float horizontalMovement = 0f;
+    private bool isRight;
+    
+    public float speed = 5f;
+    public float jumpForce=7f;
 
+     void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       // animator = GetComponent<Animator>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        //animator.SetFloat("speed", Mathf.Abs(horizontalMovement));
     }
-    public void MoveRight()
+    
+    void FixedUpdate()
     {
-       
+        Movement();   
     }
-    public void MoveLeft()
+    void Movement()
     {
-         gameObject.transform.Translate(new Vector3(-1,0,0) * speed * Time.deltaTime);
-    }
-
-    Void FixedUpdate()
-    {
-        
+        transform.position += new Vector3(horizontalMovement, 0,0) * speed * Time.deltaTime;
+        if(isRight && horizontalMovement > 0f)
+        {
+            transform.localScale = new Vector3(.25f,.25f,1);
+            isRight = false;
+            Debug.Log("right");
+        }
+        else if(!isRight && horizontalMovement < 0f)
+        {
+            transform.localScale = new Vector3(-.25f,.25f,1);
+            isRight = true;
+            Debug.Log("Left");
+        }
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f) 
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);   
+        }
     }
 }
