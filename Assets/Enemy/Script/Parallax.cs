862a46efabfd5ce3103758e7da08434bdf8d4a27
Harinesh Swaminathan <swa.harinesh@gmail.com>
2022-06-19 17:14:56 +0530

    added enemy
    yes

---
+++ Assets/Enemy/Script/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float length,startpos;
+    public GameObject cam;
+    public float parallaxeffect;
+    
+    
+    void Start()
+    {
+        startpos =transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = (cam.transform.position.x*parallaxeffect);
+        transform.position = new Vector3(startpos+dist,transform.position.y,transform.position.z);
+    }
+}
